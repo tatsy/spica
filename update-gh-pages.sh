5dd368183363ff5211968fb287ac5a6f09a69d2b
tatsy <tatsy.mail@gmail.com>
2015-04-15 18:18:38 +0900

    Fix update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-# copy rendered image to result directory
-convert ./simplept.ppm ./results/simplept.jpg
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
+  convert ./simplept.ppm $HOME/simplept.jpg
+  cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
-  git checkout gh-pages
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/tatsy/rainy.git gh-pages > /dev/null
+
+  cd gh-pages
+  cp $HOME/simplept.jpg results
 
   git add -u
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git remote set-url origin https://github.com/tatsy/rainy.git
   git push -fq origin gh-pages > /dev/null
 fi

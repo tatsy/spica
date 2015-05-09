e44bf70b61dfb185798dbe5e4d4840e3a8620cfd
tatsy <tatsy.mail@gmail.com>
2015-05-09 21:26:31 +0900

    Revise example programs to accept command line arguments.

--- example/simplemlt_example.cc
+++ example/simplemlt_example.cc
@@ -5,18 +5,24 @@ using namespace spica;
 
 
 int main(int argc, char **argv) {
-    std::cout << "Metropolis light transport" << std::endl;
+    std::cout << "*** spica: Metropolis light transport ***" << std::endl;
 
-    const int width = 320;
-    const int height = 240;
+    const int width  = argc >= 2 ? atoi(argv[1]) : 320;
+    const int height = argc >= 3 ? atoi(argv[2]) : 240;
+    const int samplePerPixel = argc >= 4 ? atoi(argv[3]) : 8;
+    const int numMLT = argc >= 5 ? atoi(argv[4]) : 8;
+
+    std::cout << "      width: " << width << std::endl;
+    std::cout << "     height: " << height << std::endl;
+    std::cout << "  sample/px: " << samplePerPixel << std::endl;
+    std::cout << "   # of MLT: " << numMLT << std::endl;
 
     Scene scene;
     Camera camera;
     cornellBoxBunny(scene, camera, width, height);
 
-    const int numMutate = 8 * width * height;
-    const int numMLT    = 8;
-    const int maxDepth  = 5;
+    const int numMutate = width * height * samplePerPixel;
+    const int maxDepth  = 7;
     Random rng = Random::getRNG();
 
     MLTRenderer renderer;
--- example/simplept_example.cc
+++ example/simplept_example.cc
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
     const int width  = argc >= 2 ? atoi(argv[1]) : 320;
     const int height = argc >= 3 ? atoi(argv[2]) : 240;
     const int samplePerPixel = argc >= 4 ? atoi(argv[3]) : 32;
+    
     std::cout << "      width: " << width << std::endl;
     std::cout << "     height: " << height << std::endl;
     std::cout << "  sample/px: " << samplePerPixel << std::endl << std::endl;

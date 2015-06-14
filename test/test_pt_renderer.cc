4fe3c0b667af1c838f84551f2a940d6b86e4a25e
tatsy <tatsy.mail@gmail.com>
2015-06-14 21:10:55 +0900

    A code for testing "path tracing renderer" is added.

---
+++ test/test_pt_renderer.cc
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+
+#include "../include/spica.h"
+using namespace spica;
+
+#include "test_macros.h"
+
+namespace {
+    const int width  = 400;
+    const int height = 300;
+    const int spp    =  16;
+
+    const std::string comp_path = DATA_DIR + "compare.bmp";
+}
+
+TEST(PTRendererTest, RenderTest) {
+    Scene scene;
+    Camera camera;
+    cornellBox(scene, camera, width, height);
+    
+    PathTracingRenderer renderer;
+    renderer.render(scene, camera, spp, QUASI_MONTE_CARLO);
+}

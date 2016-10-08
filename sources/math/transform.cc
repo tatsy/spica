11edf77a3c54ef72d1186c0ee09a729fa1597a0c
tatsy <tatsy.mail@gmail.com>
2016-10-08 11:12:14 +0900

    Revise bugs of normal handlings in Trinagle class.

--- sources/math/transform.cc
+++ sources/math/transform.cc
@@ -77,9 +77,9 @@ namespace spica {
 
     Normal3d Transform::apply(const Normal3d& n) const {
         return Normal3d(
-            mInv_(0, 0) * n.x() + mInv_(0, 1) * n.y() + mInv_(0, 2) * n.z(),
-            mInv_(1, 0) * n.x() + mInv_(1, 1) * n.y() + mInv_(1, 2) * n.z(),
-            mInv_(2, 0) * n.x() + mInv_(2, 1) * n.y() + mInv_(2, 2) * n.z()); //.normalized();
+            mInv_(0, 0) * n.x() + mInv_(1, 0) * n.y() + mInv_(2, 0) * n.z(),
+            mInv_(0, 1) * n.x() + mInv_(1, 1) * n.y() + mInv_(2, 1) * n.z(),
+            mInv_(0, 2) * n.x() + mInv_(1, 2) * n.y() + mInv_(2, 2) * n.z());
     }
 
     Vector3d Transform::apply(const Vector3d& v) const {
